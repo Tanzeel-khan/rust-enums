e0f953d95f57e38294f90920072d0d9cd2e26a40
Tanzeel-khan <ktanzeel184@gmail.com>
2020-07-06 08:59:49 +0500

    entering data directly in enum

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,21 @@ fn main() {
   
     // let student1 =Student::Online;
     // let student2 =Student::Onsite;
-    let ip_address1 = IpAddress{
-        kind : IpAddrKind::V4,
-        address: String::from("127.0.0.1")
-    };
-    let ip_address2 = IpAddress{
-        kind : IpAddrKind::V6,
-        address : String::from("127.0.0.1")
-    };
+    // let ip_address1 = IpAddress{
+    //     kind : IpAddrKind::V4,
+    //     address: String::from("127.0.0.1")
+    // };
+    // let ip_address2 = IpAddress{
+    //     kind : IpAddrKind::V6,
+    //     address : String::from("127.0.0.1")
+    // };
+    // println!("{:#?}",ip_address1);
+    // println!("{:#?}",ip_address2);
+    let ip_address1 = IpAddrKind::V4(String::from("127.0.0.2"));
+    let ip_address2 = IpAddrKind::V6(String::from("127.0.1.1"));
     println!("{:#?}",ip_address1);
     println!("{:#?}",ip_address2);
+
 }
  #[derive(Debug)]
 // enum Student{
@@ -20,11 +25,11 @@ fn main() {
 // }
 
  enum IpAddrKind {
-    V4,
-    V6
+    V4(String),
+    V6(String)
  }
- #[derive(Debug)]
- struct IpAddress{
-     kind : IpAddrKind,
-    address : String
- }
+//  #[derive(Debug)]
+//  struct IpAddress{
+//      kind : IpAddrKind,
+//     address : String
+//  }
